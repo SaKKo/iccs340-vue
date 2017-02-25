8615fe875df4984923097377cbac2db78b3959b4
sakko <saklism@gmail.com>
2017-02-25 11:59:39 +0700

    move component to topbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,6 @@
 <template>
   <div id="app">
-    <md-toolbar>
-      <md-button class="md-icon-button" @click.native="toggleLeftSidenav">
-        <md-icon>menu</md-icon>
-      </md-button>
-
-      <h2 class="md-title">My App</h2>
-    </md-toolbar>
-    <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
-      <md-toolbar class="md-large">
-        <div class="md-toolbar-container">
-          <h3 class="md-title">Sidenav content</h3>
-        </div>
-      </md-toolbar>
-      <md-list>
-        <md-list-item class="md-inset">
-          <router-link :to="{ name: 'User.sign_in'}">User</router-link>
-        </md-list-item>
-        <md-list-item class="md-inset">
-          <router-link :to="{ name: 'Posts.index'}">Posts</router-link>
-        </md-list-item>
-        <md-list-item class="md-inset">
-          <router-link :to="{ name: 'Posts.new'}">New Post</router-link>
-        </md-list-item>
-      </md-list>
-    </md-sidenav>
+    <v-top-bar></v-top-bar>
     <router-view></router-view>
   </div>
 </template>
@@ -32,22 +8,8 @@
 <script>
 export default {
   name: 'app',
-  methods: {
-    toggleLeftSidenav () {
-      this.$refs.leftSidenav.toggle()
-    },
-    toggleRightSidenav () {
-      this.$refs.rightSidenav.toggle()
-    },
-    closeRightSidenav () {
-      this.$refs.rightSidenav.close()
-    },
-    open (ref) {
-      console.log('Opened: ' + ref)
-    },
-    close (ref) {
-      console.log('Closed: ' + ref)
-    }
+  components: {
+    VTopBar: require('./components/TopBar.vue')
   }
 }
 </script>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="top-bar">
+    <md-toolbar>
+      <md-button class="md-icon-button" @click.native="toggleLeftSidenav">
+        <md-icon>menu</md-icon>
+      </md-button>
+
+      <h2 class="md-title">My App</h2>
+    </md-toolbar>
+    <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+      <md-toolbar class="md-large">
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Sidenav content</h3>
+        </div>
+      </md-toolbar>
+      <md-list>
+        <md-list-item class="md-inset">
+          <router-link :to="{ name: 'User.sign_in'}">User</router-link>
+        </md-list-item>
+        <md-list-item class="md-inset">
+          <router-link :to="{ name: 'Posts.index'}">Posts</router-link>
+        </md-list-item>
+        <md-list-item class="md-inset">
+          <router-link :to="{ name: 'Posts.new'}">New Post</router-link>
+        </md-list-item>
+      </md-list>
+    </md-sidenav>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'top-bar',
+  methods: {
+    toggleLeftSidenav () {
+      this.$refs.leftSidenav.toggle()
+    },
+    toggleRightSidenav () {
+      this.$refs.rightSidenav.toggle()
+    },
+    closeRightSidenav () {
+      this.$refs.rightSidenav.close()
+    },
+    open (ref) {
+      console.log('Opened: ' + ref)
+    },
+    close (ref) {
+      console.log('Closed: ' + ref)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
